db8ebf210849793b3575821cff593427e9af29ea
서지수 <diduedidue@naver.com>
2024-11-13 14:28:35 +0900

    ✨ Init: First Commit

---
+++ electron.vite.config.js
@@ -0,0 +1,12 @@
+export default {
+    main: {
+      // main 프로세스의 Vite 설정
+    },
+    preload: {
+      // preload 스크립트의 Vite 설정
+    },
+    renderer: {
+      // renderer 프로세스의 Vite 설정
+    }
+  }
+  
