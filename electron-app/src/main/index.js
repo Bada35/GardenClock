a2de785b6b01a7cfb2ee841ea50eabce33199fb4
서지수 <diduedidue@naver.com>
2024-11-13 15:09:14 +0900

    🚩 Feat: 기본적인 시계 구현

--- electron-app/src/main/index.js
+++ electron-app/src/main/index.js
@@ -6,8 +6,8 @@ import icon from '../../resources/icon.png?asset'
 function createWindow() {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
-    width: 900,
-    height: 670,
+    width: 400,
+    height: 180,
     show: false,
     autoHideMenuBar: true,
     ...(process.platform === 'linux' ? { icon } : {}),
